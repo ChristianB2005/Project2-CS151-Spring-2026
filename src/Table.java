e7ce2c49f8b914f27c5ca5de376c9fd17d6070e4
J.D. Bowman <thesilverbeluga@gmail.com>
2026-03-24 18:03:06 -0700

    Table class stub

---
+++ src/Table.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Table implements Reservable{
+    private static int numTables = 0;
+    private int maxCapacity;
+    private Server server;
+    private boolean isOccupied;
+    private ArrayList<Customer> customersAtTable = new ArrayList<Customer>();
+
+    public Table(int maxCapacity){
+        if (numTables >= Constants.MAXIMUM_INSTANCES){
+            throw new RuntimeException("Maximum number of Table instances reached.");
+        }
+        numTables++;
+        this.maxCapacity = maxCapacity;
+    }
+
+    public int getMaxCapacity(){
+        return this.maxCapacity;
+    }
+
+    public void assignServer(Server newServer){
+        this.server = newServer;
+    }
+
+    public boolean isOccupied(){
+        return this.isOccupied;
+    }
+
+    public void setOccupation(boolean isOccupied){
+        this.isOccupied = isOccupied;
+    }
+
+    public void clearOccupancy(){
+        this.server = null;
+        customersAtTable.clear();
+    }
+
+    public void addCustomer(Customer newCustomer){
+        customersAtTable.add(newCustomer);
+    }
+}
