4f2c66fdc7057d0f58d50480d813056f7085ecac
Christian Barber <96673461+ChristianB2005@users.noreply.github.com>
2026-03-26 19:08:03 -0700

    Delete src/util/Constants.java
    Jesse has the most up-to-date version and is being merged

--- src/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Constants {
-    public static final int MAXIMUM_INSTANCES = 100;
-    public static final String RESERVATION_STATUS_CONFIRMED = "Confirmed";
-    public static final String RESERVATION_STATUS_PENDING = "Pending";
-    public static final String RESERVATION_STATUS_CANCELLED = "Cancelled";
-    public static final String ORDER_READY = "READY";
-}
